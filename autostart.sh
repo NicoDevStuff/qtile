9cc9c82d7ac621139fb6ec68e1cb0a0308513cb3
NicoDevStuff <NicoG2008@gmx.de>
2023-04-02 16:13:38 +0200

    First commit

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+xbindkeys -f ~/.config/.xbindkeysrc &
+picom &
+nm-applet &
+volumeicon &
+conky -c ~/.config/conky/qtile/conky.conf
+~/.screenlayout/main.sh &
+udiskie &
+easyeffects &
+solaar &
+xclip &
+nitrogen --restore &  
---
+++ config.py
@@ -0,0 +1,248 @@
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+import subprocess
+import os
+
+mod = "mod4"
+terminal = "kitty"
+browser = "firefox"
+
+keys = [
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
+    Key([mod, "control"], "h", 
+        lazy.layout.shrink(),
+        lazy.layout.decrease_nmaster(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", 
+        lazy.layout.grow(), 
+        lazy.layout.increase_nmaster(),
+        desc="Grow window to the right"),
+
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+   
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+
+    # My keybinds 🗿🗿🗿🗿
+    Key([mod],"Return",             lazy.spawn(terminal),           desc="Launch terminal"),
+    Key([mod, "shift"], "Return",   lazy.spawn("rofi -show drun"),  desc="Application Launcher"),
+    Key([mod, "shift"], "c",        lazy.spawn("rofi -show calc"),  desc="Launch calculator"),
+    Key([mod, "shift"], "e",        lazy.spawn("emoji-picker"),     desc="Launch emojis"),
+    Key([mod, "shift"], "s",        lazy.spawn("signal-desktop"),   desc="Launch signal"),
+    Key([mod]         , "b",        lazy.spawn(browser),            desc="Launch firefox"),
+    Key([mod]         , "e",        lazy.spawn("thunderbird"),      desc="Launch thunderbird"),
+    Key([mod]         , "f",        lazy.spawn("pcmanfm"),          desc="Launch thunderbird"),
+    Key([mod]         , "q",        lazy.window.kill(),             desc="Kill focused window"),
+    Key([mod, "shift"], "f",        lazy.window.toggle_floating(),  desc="float focused window"),
+    Key([mod, "shift"], "r",        lazy.reload_config(),           desc="Reload the config"),
+    Key([mod, "shift"], "x",        lazy.shutdown(),                desc="Shutdown Qtile"),
+    
+    # Media keys
+    Key([ ], 'XF86AudioMicMute',    lazy.spawn('pactl set-source-mute @DEFAULT_SOURCE@ toggle')),
+    Key([ ], 'XF86AudioMute',       lazy.spawn('pactl set-sink-mute @DEFAULT_SINK@ toggle')),
+    Key([ ], 'XF86AudioLowerVolume',lazy.spawn('pactl set-sink-volume @DEFAULT_SINK@ -1000')),
+    Key([ ], 'XF86AudioRaiseVolume',lazy.spawn('pactl set-sink-volume @DEFAULT_SINK@ +1000')),
+    Key([ ], 'XF86AudioPlay',       lazy.spawn('playerctl play-pause')),
+    Key([ ], 'XF86AudioPrev',       lazy.spawn('playerctl previous')),
+    Key([ ], 'XF86AudioNext',       lazy.spawn('playerctl next')),
+    Key([ ], 'XF86MonBrightnessUp', lazy.spawn('brightnessctl set +5%')),
+    Key([ ], 'XF86MonBrightnessDown',lazy.spawn('brightnessctl set 5%-')),
+
+    # Caps lock indicator
+    Key([ ], 'Caps_Lock',           lazy.spawn('notify-send "🤓🤓🤓🤓" "<u><b>CAPS</b></u>" -u critical')),
+
+]
+
+groups = [Group(i) for i in "123456789"]
+
+for i in groups:
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod1 + shift + letter of group = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
+
+colors = [["#282828", "#282828"],
+          ["#1d2021", "#1d2021"],
+          ["#ebdbb2", "#ebdbb2"],
+          ["#fb4934", "#fb4934"],
+          ["#b8bb26", "#b8bb26"],
+          ["#fe8019", "#fe8019"],
+          ["#458588", "#458588"],
+          ["#b16286", "#b16286"],
+          ["#83a598", "#83a598"],
+          ["#d3869b", "#d3869b"],
+          ["#bdae93", "#bdae93"]]
+
+layout_theme = {"border_width": 2,
+                "margin": 8,
+                "border_focus": colors[6],
+                "border_normal": colors[0]
+                }
+layouts = [
+    layout.MonadTall(**layout_theme),
+    layout.Max(),
+    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+    layout.Floating(**layout_theme)
+]
+
+widget_defaults = dict(
+    font="JetBrains Mono",
+    fontsize=14,
+    margin_y = 4,
+    background=colors[1]
+)
+extension_defaults = widget_defaults.copy()
+
+def init_widgets_list():
+    widgets_list = [
+        widget.GroupBox(
+            font = "JetBrains Mono",
+            fontsize = 15,
+            margin_y = 4,
+            margin_x = 0,
+            padding_y = 0,
+            padding_x = 6,
+            borderwidth = 3,
+            active = colors[2],
+            inactive = colors[10],
+            rounded = False,
+            highlight_color = colors[1],
+            highlight_method = "line",
+            this_current_screen_border = colors[6],
+            this_screen_border = colors [4],
+            other_current_screen_border = colors[6],
+            other_screen_border = colors[4],
+            foreground = colors[2],
+            background = colors[0]
+            ),
+        
+        widget.Sep(
+               linewidth = 5, padding = 0,
+               foreground = colors[0], background = colors[0]
+               ),
+        widget.WindowName(),
+        widget.Chord(
+            chords_colors={
+                "launch": ("#ff0000", "#ffffff"),
+            },
+            name_transform=lambda name: name.upper(),
+        ),
+        widget.Clock(format="%Y-%m-%d %a %H:%M:%S %p"),
+        widget.Systray(),
+    ]
+    return widgets_list
+
+def init_widgets_screen1():
+    widgets_screen1 = init_widgets_list()
+    del widgets_screen1[6:7]               # Slicing removes unwanted widgets (systray) on Monitors 1,3
+    return widgets_screen1
+
+def init_widgets_screen2():
+    widgets_screen2 = init_widgets_list()
+    return widgets_screen2                 # Monitor 2 will display all widgets in widgets_list
+
+def init_screens():
+    return [Screen(top=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=20)),
+            Screen(top=bar.Bar(widgets=init_widgets_screen2(), opacity=1.0, size=20))]
+
+if __name__ in ["config", "__main__"]:
+    screens = init_screens()
+    widgets_list = init_widgets_list()
+    widgets_screen1 = init_widgets_screen1()
+    widgets_screen2 = init_widgets_screen2()
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# Autostarts
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    subprocess.Popen([home])
+
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
+
+
